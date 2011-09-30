1277291cc309dc436116315b4af4ca054f5624b5
nguyenhaiquan@gmail.com <nguyenhaiquan@gmail.com@cf5a5b9d-6ff9-a1ac-40a9-9ec27b63fe2e>
2011-09-30 13:34:29 +0000

    new Indicator

---
+++ trunk/main/Indicators/Class/TRIX.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Drawing;
+using System.Text;
+using TicTacTec.TA.Library;
+using application;
+
+namespace Indicators
+{
+    /// <summary>
+    /// CCI Helper class
+    /// </summary>
+    public class TRIXHelper : Helpers
+    {
+        public TRIXHelper()
+        {
+            Init(typeof(TRIX), typeof(forms.commonForm));
+        }
+    }
+
+    /// <summary>
+    /// CCI indicator
+    /// </summary>
+    public class TRIX : DataSeries
+    {
+        /// <summary>
+        /// Static method to create Arron DataSeries
+        /// </summary>
+        /// <param name="ds"></param>
+        /// <param name="period"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static TRIX Series(DataSeries ds, int period, string name)
+        {
+            //Build description
+            string description = "(" + name + period.ToString() + ")";
+            //See if it exists in the cache
+            object obj = ds.Cache.Find(description);
+            if (obj != null) return (TRIX)obj;
+
+            //Create indicator, cache it, return it
+            TRIX indicator = new TRIX(ds, period, description);
+            ds.Cache.Add(description, indicator);
+            return indicator;
+        }
+
+        /// <summary>
+        /// Calculation of CCI indicators
+        /// </summary>
+        /// <param name="ds">data to calculate CCI</param>        
+        /// <param name="period">period to calculate</param>
+        /// <param name="name"></param>
+        public TRIX(DataSeries ds, int period, string name)
+            : base(ds, name)
+        {
+            int begin = 0, length = 0;
+            Core.RetCode retCode = Core.RetCode.UnknownErr;
+
+            double[] output = new double[ds.Count];
+
+            retCode = Core.Trix(0, ds.Count - 1, ds.Values, period, out begin, out length, output);
+
+            if (retCode != Core.RetCode.Success) return;
+            //Assign first bar that contains indicator data
+            FirstValidValue = begin;
+            this.Name = name;
+
+            for (int i = begin, j = 0; j < length; i++, j++)
+                this[i] = output[j];
+        }
+    }
+}
---
+++ trunk/main/Indicators/Class/VAR.cs
@@ -0,0 +1,68 @@
+﻿using TicTacTec.TA.Library;
+using application;
+
+namespace Indicators
+{
+    /// <summary>
+    /// CCI Helper class
+    /// </summary>
+    public class VARHelper : Helpers
+    {
+        public VARHelper()
+        {
+            Init(typeof(VAR), typeof(forms.commonForm));
+        }
+    }
+
+    /// <summary>
+    /// CCI indicator
+    /// </summary>
+    public class VAR : DataSeries
+    {
+        /// <summary>
+        /// Static method to create Arron DataSeries
+        /// </summary>
+        /// <param name="ds"></param>
+        /// <param name="period"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static VAR Series(DataSeries ds, int period,double NbDev, string name)
+        {
+            //Build description
+            string description = "(" + name + period.ToString() + ")";
+            //See if it exists in the cache
+            object obj = ds.Cache.Find(description);
+            if (obj != null) return (VAR)obj;
+
+            //Create indicator, cache it, return it
+            VAR indicator = new VAR(ds, period,NbDev, description);
+            ds.Cache.Add(description, indicator);
+            return indicator;
+        }
+
+        /// <summary>
+        /// Calculation of VAR indicators
+        /// </summary>
+        /// <param name="db">data to calculate VAR</param>        
+        /// <param name="period">period to calculate</param>
+        /// <param name="name"></param>
+        public VAR(DataSeries db, int period, double optInNbDev, string name)
+            : base(db, name)
+        {
+            int begin = 0, length = 0;
+            Core.RetCode retCode = Core.RetCode.UnknownErr;
+
+            double[] output = new double[db.Count];
+
+            retCode = Core.Variance(0, db.Count - 1, db.Values, period, optInNbDev ,out begin, out length, output);
+
+            if (retCode != Core.RetCode.Success) return;
+            //Assign first bar that contains indicator data
+            FirstValidValue = begin;
+            this.Name = name;
+
+            for (int i = begin, j = 0; j < length; i++, j++)
+                this[i] = output[j];
+        }
+    }
+}
