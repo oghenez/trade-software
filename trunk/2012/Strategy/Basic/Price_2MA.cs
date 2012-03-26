5615848d7ae43e633f47a544af194c229eb670b8
nguyenhaiquan@gmail.com <nguyenhaiquan@gmail.com@cf5a5b9d-6ff9-a1ac-40a9-9ec27b63fe2e>
2012-03-26 08:48:08 +0000

    small change, info for strategy

--- trunk/2012/Strategy/Basic/Price_2MA.cs
+++ trunk/2012/Strategy/Basic/Price_2MA.cs
@@ -1,5 +1,6 @@
 ﻿using application.Strategy;
 using commonClass;
+using commonTypes;
 
 namespace Strategy
 {
@@ -13,13 +14,36 @@ namespace Strategy
         override protected void StrategyExecute()
         {
             PriceTwoSMARule smarule = new PriceTwoSMARule(data.Close, parameters[0], parameters[1]);
+
+            int cutlosslevel = 5;
+            Indicators.MIN min = Indicators.MIN.Series(data.Close, parameters[1], "min");
+            Indicators.MAX max = Indicators.MAX.Series(data.Close, parameters[2], "max");
+            Indicators.Fibonnanci fibo = Indicators.Fibonnanci.Series(data.Bars, parameters[1], "fibo");
+
+
             for (int idx = smarule.long_indicator.FirstValidValue; idx < smarule.long_indicator.Count; idx++)
             {
                 if (smarule.isValid_forBuy(idx))
-                    BuyAtClose(idx);
+                {
+                    BusinessInfo info = new BusinessInfo();
+                    info.SetTrend(AppTypes.MarketTrend.Upward, AppTypes.MarketTrend.Unspecified, AppTypes.MarketTrend.Unspecified);
+                    info.Short_Resistance = max[idx];
+                    info.Short_Support = min[idx];
+                    info.Short_Target = fibo.Fibo23pc[idx];
+                    info.Stop_Loss = data.Close[idx] * (1 - cutlosslevel / 100);
+                    BuyAtClose(idx,info);
+                }
                 else
-                if (smarule.isValid_forSell(idx))
-                    SellAtClose(idx);
+                    if (smarule.isValid_forSell(idx))
+                    {
+                        BusinessInfo info = new BusinessInfo();
+                        info.SetTrend(AppTypes.MarketTrend.Upward, AppTypes.MarketTrend.Unspecified, AppTypes.MarketTrend.Unspecified);
+                        info.Short_Resistance = max[idx];
+                        info.Short_Support = min[idx];
+                        info.Short_Target = fibo.Fibo23pc[idx];
+                        info.Stop_Loss = data.Close[idx] * (1 - cutlosslevel / 100);
+                        SellAtClose(idx,info);
+                    }
             }
         }
     }
