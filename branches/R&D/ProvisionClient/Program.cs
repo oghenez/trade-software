fa07230ab84821df6ddb43a6987fe66f468fd92c
nguyenhaiquan@gmail.com <nguyenhaiquan@gmail.com@cf5a5b9d-6ff9-a1ac-40a9-9ec27b63fe2e>
2013-07-07 11:49:29 +0000

    Thu muc nghien cuu ve SyncData

---
+++ branches/R&D/ProvisionClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Data.SqlClient;
+using System.Data.SqlServerCe;
+
+using Microsoft.Synchronization.Data;
+using Microsoft.Synchronization.Data.SqlServer;
+using Microsoft.Synchronization.Data.SqlServerCe;
+using System;
+
+namespace ProvisionClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a connection to the SyncCompactDB database
+            try
+            {
+                //SqlCeConnection clientConn = new SqlCeConnection(@"Data Source='C:\Users\quan_nh\Documents\Visual Studio 2008\Projects\SyncApplication130702\ProvisionClient\SyncCompactDB.sdf'");
+                SqlCeConnection clientConn = new SqlCeConnection("Data Source='C:\\SyncCompactDB.sdf'");
+                //clientConn.ServerVersionSqlCeEngine.
+                clientConn.Open();
+                // create a connection to the SyncDB server database
+                SqlConnection serverConn = new SqlConnection("Data Source=HAIQUAN\\SQLEXPRESS; Initial Catalog=SyncDB; Integrated Security=True");
+
+                //// get the description of ProductsScope from the SyncDB server database
+                DbSyncScopeDescription scopeDesc = SqlSyncDescriptionBuilder.GetDescriptionForScope("ProductsScope", serverConn);
+
+                //// create CE provisioning object based on the ProductsScope
+                //SqlCeSyncScopeProvisioning clientProvision;
+                //clientProvision = new SqlCeSyncScopeProvisioning(clientConn, scopeDesc);
+
+                //// starts the provisioning process
+                //clientProvision.Apply();
+            }
+            catch (Exception e)
+            {
+                System.Console.Write(e.Message);
+            }
+        }
+    }
+}
---
+++ branches/R&D/SyncApplication130702/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Synchronization;
+using Microsoft.Synchronization.Files;
+
+namespace SyncApplication130702
+{
+    public partial class Form1 : Form
+    {
+        Guid sourceReplicaID;
+        Guid destReplicaID;
+        String sourceFolder = "D:\\Temp\\Source";
+        String destFolder = "D:\\Temp\\Dest";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSyncFiles_Click(object sender, EventArgs e)
+        {
+            btnSyncFiles.Enabled = false;
+            lblStatus.Text="Starting...";
+            FileSyncProvider sourceProvider=new FileSyncProvider(sourceReplicaID,@sourceFolder);
+            FileSyncProvider destProvider=new FileSyncProvider(destReplicaID,@destFolder);
+
+            //sync agent
+            SyncOrchestrator synAgent=new SyncOrchestrator();
+            synAgent.LocalProvider=sourceProvider;
+            synAgent.RemoteProvider=destProvider;
+            synAgent.Synchronize();
+            lblStatus.Text="Finish...";
+            btnSyncFiles.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sourceReplicaID = Guid.NewGuid();
+            destReplicaID = Guid.NewGuid();
+        }
+    }
+}
